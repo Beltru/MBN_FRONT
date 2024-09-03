ee535992831f12c26c5a575eb846353eb442d47c
Beltru <belvader007@gmail.com>
2024-09-03 10:06:53 -0300

    siguiendo navbar

--- mbn/app/page.jsx
+++ mbn/app/page.jsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
+  <div className="pt-20 min-h-screen bg-gradient-to-r from-[#0f0015] via-[#2c003f] to-black text-white p-6 overflow-hidden">
     <header>
       <nav className="fixed bottom-[5vh] mx-[20vw] bg-black/70 w-[55vw] rounded-xl px-2 py-1">
         <div className="flex flex-row items-center">
@@ -37,9 +38,13 @@ export default function Home() {
                 height={18}
               />
             </li>
+            <li className="bg-black p-2 rounded-lg flex items-center justify-center hover:hover:bg-gray-600 rounded-lg mb-2 duration-500">
+            Support
+            </li>
           </ul>
         </div>
       </nav>
     </header>
+  </div>
   );
 }
