f2ac5f570d4123f3e2ecfaee00487113fcdcad66
Beltru <belvader007@gmail.com>
2024-09-03 12:06:58 -0300

    Video godeano

--- mbn/app/page.jsx
+++ mbn/app/page.jsx
@@ -60,8 +60,8 @@ export default function Home() {
     </header>
     <main>
       <section className="grid grid-rows-3 grid-cols-2 h-[100vh] w-[100vw] pl-[6vw] py-[8vh] pb-[16vh] gap-[16vw]">
-        <aside className="row-span-3 col-span-1 bg-[#CCCCCC] rounded-2xl">
-          <video src="/graph.mov" className="w-44 h-44" controls autoPlay loop></video>
+        <aside className="row-span-3 col-span-1 bg-black rounded-2xl flex items-center justify-center">
+          <video src="/graph.mp4" className="w-[50vw] h-[50vh]" autoPlay loop></video>
         </aside>
       </section>
     </main>
