dc3b9600ca369d176c8787fe2467ee9557c61e2d
Beltru <147621160+Beltru@users.noreply.github.com>
2024-09-09 14:31:19 -0300

    Add files via upload

--- mbn/app/page.jsx
+++ mbn/app/page.jsx
@@ -1,63 +1,12 @@
-import Image from "next/image";
 import ScrollCards from "../components/Cards-Scroll";
+import Navbar from "../components/Navbar-Abajo"
 
 export default function Home() {
+
   return (
-  <div className="h-[200vh] bg-gradient-to-r from-[#0f0015] via-[#080c2c] to-black overflow-hidden">
+  <div className="h-[300vh] bg-gradient-to-r from-[#0f0015] via-[#080c2c] to-black overflow-hidden">
     <header>
-      <nav className="flex justify-between flex-row items-center fixed bottom-[5vh] mx-[20vw] bg-[rgb(26,26,26,0.8)] w-[55vw] rounded-xl px-4 py-1 ">
-        <div className="flex flex-row items-center">
-          <div>
-            <Image
-              src={"/mbn.png"}
-              width={55}
-              height={55}
-            />
-          </div>
-          <div className="divider">
-            <hr className="line mx-2 mr-2"/>
-          </div>
-          <ul className="flex flex-row">
-            <li className=" p-2 rounded-lg flex items-center justify-center hover:bg-[#303030] transition-all duration-500 cursor-pointer mx-1">
-              <Image 
-                src={"/bank.svg"}
-                width={18}
-                height={18}
-              />
-            </li>
-            <li className=" p-2 rounded-lg flex items-center justify-center hover:bg-[#303030] transition-all duration-500 cursor-pointer mx-1">
-              <Image 
-                src={"/graph-up.svg"}
-                width={18}
-                height={18}
-              />
-            </li>
-            <li className=" p-2 rounded-lg flex items-center justify-center hover:bg-[#303030] transition-all duration-500 cursor-pointer mx-1">
-              <Image 
-                src={"/wallet2.svg"}
-                width={18}
-                height={18}
-              />
-            </li>
-          </ul>
-        </div>
-        <div className="flex flex-row items-center">
-          <ul className="flex flex-row">
-            <li className="px-2 py-1 rounded-lg flex items-center justify-center hover:hover:bg-[#303030] duration-500 cursor-pointer mr-4 text-[0.95rem] font-medium">
-              Team
-            </li>
-            <li className="px-2 py-1 rounded-lg flex items-center justify-center hover:hover:bg-[#303030] duration-500 cursor-pointer mr-4 text-[0.95rem] font-medium">
-              Support
-            </li>
-            <li className="px-2 py-1 rounded-lg flex items-center justify-center hover:hover:bg-[#303030] duration-500 cursor-pointer mr-4 text-[0.95rem] font-medium">
-              Login
-            </li>
-            <li className="px-3 py-1 rounded-lg flex items-center justify-center bg-red-600 hover:hover:bg-red-700 duration-500 cursor-pointer mr-2 text-[0.95rem] font-bold">
-              Sign Up
-            </li> 
-          </ul>
-        </div>
-      </nav>
+      <Navbar/>
     </header>
     <main className="mt-[70vh]">
       <ScrollCards  />
